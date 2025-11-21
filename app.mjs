83a624df5380ac65c047cb935dbe3bb3a3a08b73
정마나미 <igarimanami032729@gmail.com>
2025-11-21 09:35:28 +0900

    초기 세팅

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import postsRouter from "./router/posts.mjs";
+import authRouter from "./router/auth.mjs";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/post", postsRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.listen(8080); // 웹 사이트 포트는 8000부터
