6041447310710f528c3ec1100dd3ca698cd42bfe
정마나미 <igarimanami032729@gmail.com>
2025-11-21 12:34:01 +0900

    배열 데이터 API 데스트 -유저 추가

--- app.mjs
+++ app.mjs
@@ -9,6 +9,12 @@ app.use(express.json());
 app.use("/post", postsRouter);
 app.use("/auth", authRouter);
 
+// app.use("/auth", (req, res, next) => {
+//     app.post("/signup", )
+//     app.post("/login", )
+//     ...
+// });
+
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
--- router/auth.mjs
+++ router/auth.mjs
@@ -1,10 +1,20 @@
 import express from "express";
+import * as authController from "../controller/auth.mjs";
 
 const router = express.Router();
+// router.post("/signup", (req, res, next) => {
+//   const { userid, password, name, email } = req.body;
+//   const user = authRepository.createUser(userid, password, name, email);
+//   if (user) {
+//     res.status(200).json(user);
+//   }
+// });
 
 // 회원 가입
+router.post("/signup", authController.signup);
 
 // 로그인
+router.post("/login", authController.login);
 
 // 로그인 유지
 
